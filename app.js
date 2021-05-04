87e164120068aac193b462b26a367fa1b102ec71
Отгоо <jf15017583402@gmail.com>
2021-05-04 10:06:31 +0800

    Toglogchiin onoo tsugluuldag bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Toglogchiin eeljiig hadgalah huvisagch, negdugeer toglogchiig 0, hoyordugaar toglogchiig 1 gej temdeglie
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Toglogchiintsugluulsan onoog hadgalah huvisagch
 var score = [0, 0];
@@ -15,8 +15,27 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var docdom = document.querySelector(".dice");
 docdom.style.display = "none";
+//Shoog shideh event listner
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 hoorond sansanamsargui toog
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  //web deer bgaa zuragiig haragddag bolgoh
   docdom.style.display = "block";
+  //shoonii toog zurgand hargalzan gargah
   docdom.src = "dice-" + diceNumber + ".png";
+  if (diceNumber !== 1) {
+    // yalgaatai bol toglogchiin onoog nemeh
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    roundScore = 0;
+    activePlayer === 1 ? (activePlayer = 0) : (activePlayer = 1);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+  }
 });
+//
+// // Shoond buusan eeljit toglogchiin onoond nemj ugnu gehdee 1 busad toog l n     emene.
+
+// //1 buuhad eeljin toglogchiig solino}
